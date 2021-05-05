5c4648181094344b5fa0fbd54be1189ff1dda274
Henrique Amorim Guarnieri <56804552+oRiqueee@users.noreply.github.com>
2021-05-05 19:10:40 -0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,21 @@
+const token = "Token"; // Token do seu bot
+const chatId = "ID"; // ID do chat no qual a mensagem deve ser enviada
+const minhaMensagem = "Mensagem"; // Mensagem que deseja enviar
+const intervaloEnvio = 120000; // Intervalo de envio das mensagens em milessegundos
+
+const linkApi = "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatId + "&text=" + minhaMensagem;
+var qntID = document.getElementById('mensagens');
+var qntMensagem = 1;
+
+setInterval(() => {
+
+    fetch(linkApi)
+  .then(response => response.json())
+  .then(data =>{
+  
+  qntID.innerText = qntMensagem++;
+  
+  
+  })
+
+}, intervaloEnvio);
